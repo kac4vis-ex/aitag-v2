cd23a3c9d55aa8f811d06b99676beb792b5fcb09
kac4vis-ex <kac4vis@gmail.com>
2026-06-04 11:07:44 +0530

    Test1 (#1)
    * AI-Lines-Of-Code: 20
    Manual-LOC: 3
    
    * m-2
    
    AI-Lines-Of-Code: 0
    Manual-LOC: 2
    
    * 4-2
    
    AI-Lines-Of-Code: 0
    Manual-LOC: 6
    
    * add
    
    AI-Lines-Of-Code: 0
    Manual-LOC: 0

--- src/sample.js
+++ src/sample.js
@@ -36,3 +36,13 @@ console.log('AI test line 2');
 console.log('AI test line 1');
 console.log('AI test line 2');
 console.log('AI test line 2');
+console.log('AI test line 1');
+console.log('AI test line 2');
+console.log('AI test line 1');
+console.log('AI test line 2');
+console.log('AI test line 1');
+console.log('AI test line 2');
+// START OF AI GENERATED
+console.log('AI test line 1');
+console.log('AI test line 2');
+// END OF AI GENERATED
